3ba1a6cdcf008fa3b1744db59eb40359750ff71f
John Whitten <johnwhitten.studio@gmail.com>
2022-01-27 13:10:58 -0800

    delete comments and clean up code

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import MapService from './map-service.js';
 import MealService from './meal-service.js';
 import DrinkService from './drink-service.js';
 
+// ------------------------WEATHER & MAP LOGIC------------------------ //
+
 function forecastByDay(response) {
   let forcastDateAndTime = "";
   for (let i = 0; i<response.list.length; i++) {
@@ -48,10 +50,9 @@ $(document).ready(function() {
   });
 });
 
-//data tab target links two together 
-//loop through the tabs and adding an event listener and show the target
-//data tab congit comtent, you going to loop thru, add classList of remove to remove the active 
-//21Attributes can be set and read by the camelCase name/key as an object property of the dataset: element.dataset.keyname
+// ------------------------DRAGGABLE LOGIC------------------------ //
+
+
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
 
@@ -69,12 +70,11 @@ tabs.forEach(tab => {
   });
 });
 
-//draggables
-//first grab all the things you can grab and containers is where you can drop elements
+
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
-//loop through each draggable and add and EventListener.  1st you have to start it with dragstart event. add to class using .classList, add dragging. then stop ghost like effect by classlist.remove 
+
 draggables.forEach(draggable => {
   draggable.addEventListener('dragstart', () => {
     draggable.classList.add('dragging');
@@ -84,8 +84,7 @@ draggables.forEach(draggable => {
     draggable.classList.remove('dragging');
   });
 });
-//looping through the container.  dragover will check if over something. then check to see what which container it is in.  draggable is the one you are draggin and it will add to the container 1 vs 2. e preventdefault is to drop it in the element/container 
-//if afterElements is not anything then goes on the bottom of the list 
+
 containers.forEach(container => {
   container.addEventListener('dragover', e => {
     console.log ('dragover'); 
@@ -99,9 +98,7 @@ containers.forEach(container => {
     }
   });
 });
-//this will put it in a certain position (not just containers). to get all of the draggable elements (container.querySelectorAll) but to return it into a array use the spread operator 
-//closest and  every one is a child of the container
-//offset (middle of the box) is the to figure out what it is closest to.  if less than zero it is above, if closest to offset and barely above element 
+
 function getDragAfterElement(container, y) {
   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
@@ -118,19 +115,11 @@ function getDragAfterElement(container, y) {
 
 
 
-// ----------------------------Meal Service and Drink Service Logic ---------------------------- //
+// ------------------------MEALS & COCKTAILS LOGIC------------------------ //
 
-// function clearFields(){
-//   $('#search').val("");
-//   $('#imageToShow').attr("");
-//   $('.showName').text("");
-//   $('.showCategory').text("");
-//   $('.showArea').text("");
-// }
 
 $('#mealSearch').click(function() {
   let search1 = $('#search1').val();
-  // clearFields();
   let promise = MealService.getSearch(search1);
   promise.then(function(response){
     const body = JSON.parse(response);
@@ -228,13 +217,11 @@ $('#mealSearch').click(function() {
     // Instructions
     let mealInstructions = body.meals[0].strInstructions;
     $('.showInstructions').text(mealInstructions);
-    // $('.footer').show();
   });
 });
 
 $('#drinkSearch').click(function() {
   let search2 = $('#search2').val();
-  // clearFields();
   let promise = DrinkService.getSearch(search2);
   promise.then(function(response){
     const body = JSON.parse(response);
@@ -276,4 +263,3 @@ $('#drinkSearch').click(function() {
   });
 });
 
-// ----------------------------Meal Service and Drink Service Logic ---------------------------- //
